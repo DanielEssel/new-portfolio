5f44d7c1879b2ecd6567b69b192ef2ba50bf3793
DanielEssel <dessel731@outlook.com>
2024-09-19 21:10:10 +0000

    all values

---
+++ script.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablinks.classList.remove("active-links"); 
+            }
+        function opentab(tabename){
+            for(tabcontent for tabcontents){
+                tabcontent.classList.remove("active-tab"); 
+            }
+        }
